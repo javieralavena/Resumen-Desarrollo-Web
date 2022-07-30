9d12437425a13c548773aae4c327177425742a22
Javiera <javierabta97@gmail.com>
2022-07-29 21:47:28 -0400

    Añadi la estructura base de HTML y lo hice responsivo

---
+++ docs/.vuepress/components/EstructuraHTML.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="estructura">
+
+        <div class="estructura-imagen">
+            <img src="/Estructura-HTML.png" alt="Estructura Base de HTML">
+        </div>
+
+        <div class="estructura-base">
+
+            <p class="doctype"> <span> DOCTYPE →</span> Indica al navegador como debe interpretar el documento.</p>
+            <p> <span> HTML →</span> Envuelve todo el contenido de la página, es el elemento raíz y la etiqueta
+                principal.</p>
+            <p> <span> HEAD →</span> Aquí va la información para el navegador.</p>
+            <p> <span> BODY →</span> Aquí va el contenido para mostrar al usuario.</p>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'component-estructuraHTML',
+    // props: {},
+    data: function () {
+        return {}
+    },
+    // computed: {},
+    methods: {
+        // -- UI Modification
+        // -- Data fetching
+        // -- Data handling and transformations
+        // -- Validations and Error handling
+        // -- Other functions
+        // -- Vuex mappings
+    },
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+span {
+    font-weight: bold;
+    color: #83749b;
+}
+
+p {
+    text-align: justify;
+}
+
+.estructura {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+    margin-top: 20px;
+    grid-gap: 10px;
+}
+
+.estructura-imagen img {
+    border-radius: 5%;
+}
+
+.estructura-base {
+    display: grid;
+    align-content: center;
+}
+
+@media (max-width: 1150px) {
+
+    .estructura {
+        display: grid;
+        grid-template-columns: 100%;
+        grid-template-rows: 1fr 0.5fr;
+        margin-top: 20px;
+        grid-gap: 10px;
+    }
+
+    .estructura-imagen {
+        display: grid;
+        justify-content: center;
+    }
+
+    .estructura-base {
+        display: grid;
+        align-content: start;
+        justify-content: center;
+    }
+
+    p {
+    margin: 5px 0px;
+}
+}
+</style>
--- docs/frontend/HTML.md
+++ docs/frontend/HTML.md
@@ -1,1 +1,13 @@
+
 # HTML
+
+**Lenguaje de Marcas de Hipertexto (HyperText Markup Language)**
+
+<p style="text-align: justify">Nos permite describir el <span style="font-weight: bold; color: #83749b"> formato </span> y <span style="font-weight: bold; color: #83749b"> estructura </span> de una página web, mediante <span style="font-weight: bold; color: #83749b"> etiquetas</span>. Este un estándar adoptado por todos los desarrolladores que, mediante etiquetas de texto, le permite a los navegadores interpretar los elementos de una página web.</p>
+
+## Estructura base de un documento HTML
+
+<EstructuraHTML/>
+
+## Sintaxis de una Etiqueta
+
